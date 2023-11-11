5df14147ab2b2f9f9203af25eced28cb2c516c23
Justin <justin.gawad@gmail.com>
2023-11-10 17:12:03 -0800

    added file

---
+++ docker-install.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+#
+#type all your command to install docker here
+#
+#first give permission to user i.e
+# chomd +x docker-install.sh
+#
+# to execute the file ./docker-install.sh
+#
+#
+#
+#
+#
+#
---
+++ variables.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#string
+name="swizel"
+echo "hello, $name how are you"
+
+
+abc="jatin"
+echo "how are you $abc"
+
+
+
+#interger
+age=35
+echo "whats your appro $age of jatin"
+
+
+x=4
+y=5
+sum = $(x+y)
+echo "sum: $sum" 
+
+
+
+
+
